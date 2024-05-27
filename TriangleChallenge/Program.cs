7cf872adb9519818db7de98b2cc034334709ae26
Vinicius Magalhães <magalhaesvinicius2014@gmail.com>
2024-05-27 01:40:11 -0300

    Adicionar arquivos de projeto.

---
+++ TriangleChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TrianguloMaximo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de linhas do triângulo:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] triangulo = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                triangulo[i] = new int[i + 1];
+
+                Console.WriteLine($"Digite os {i + 1} números da linha {i + 1}, separados por vírgula:");
+                string linha = Console.ReadLine();
+                string[] numeros = linha.Split(',');
+
+                if (numeros.Length != i + 1)
+                {
+                    Console.WriteLine($"Você deve fornecer exatamente {i + 1} números para a linha {i + 1}.");
+                    i--; 
+                    continue;
+                }
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    triangulo[i][j] = Convert.ToInt32(numeros[j]);
+                }
+            }
+
+            Triangulo tri = new Triangulo();
+            int resultado = tri.ResultadoTriangulo(triangulo);
+
+            Console.WriteLine($"O resultado máximo é: {resultado}");
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ TriangleChallenge/Triangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TrianguloMaximo
+{
+    public class Triangulo
+    {
+        public int ResultadoTriangulo(int[][] dadosTriangulo)
+        {
+            if (dadosTriangulo == null || dadosTriangulo.Length == 0)
+                return 0;
+
+            int n = dadosTriangulo.Length;
+
+            int[][] dp = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = new int[dadosTriangulo[i].Length];
+            }
+
+            for (int i = 0; i < dadosTriangulo[n - 1].Length; i++)
+            {
+                dp[n - 1][i] = dadosTriangulo[n - 1][i];
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < dadosTriangulo[i].Length; j++)
+                {
+                    dp[i][j] = dadosTriangulo[i][j] + Math.Max(dp[i + 1][j], dp[i + 1][j + 1]);
+                }
+            }
+
+            return dp[0][0];
+        }
+    }
+}
